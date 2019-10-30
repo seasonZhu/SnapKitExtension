6fd424186cac3a200d1bd1e8ca8dd944d8b8e8e3
zhujilong <zhujilong2007@sina.com>
2019-10-30 11:41:55 +0800

    Initial Commit

---
+++ Sources/SnapKitForViews/SnapKitForViews.swift
@@ -0,0 +1,3 @@
+struct SnapKitForViews {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SnapKitForViewsTests
+
+var tests = [XCTestCaseEntry]()
+tests += SnapKitForViewsTests.allTests()
+XCTMain(tests)
