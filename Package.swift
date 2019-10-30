525ff4364b73d258bac71bb1a89022671b8d2a9c
zhujilong <zhujilong2007@sina.com>
2019-10-30 13:46:15 +0800

    创建Swift Package

--- Package.swift
+++ Package.swift
@@ -4,25 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SnapKitForViews",
+    name: "SnapKitExtension",
+    platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SnapKitForViews",
-            targets: ["SnapKitForViews"]),
+            name: "SnapKitExtension",
+            targets: ["SnapKitExtension"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SnapKitForViews",
-            dependencies: []),
-        .testTarget(
-            name: "SnapKitForViewsTests",
-            dependencies: ["SnapKitForViews"]),
-    ]
+         .target(
+            name: "SnapKitExtension",
+            dependencies: ["SnapKit"],
+            path: "Source")],
+    swiftLanguageVersions: [.v4_2]
 )
---
+++ Source/ConstraintArray+Extensions.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SnapKit
+
+extension Array where Element: ConstraintView {
+    public var snp: ConstraintArrayDSL {
+        return ConstraintArrayDSL(array: self)
+    }
+}
+
+extension String {
+    func size(withFont font: UIFont) -> CGSize {
+        let attributes = [NSAttributedString.Key.font: font]
+        return (self as NSString).size(withAttributes: attributes)
+    }
+}
+
